b486227bff3aff09746cd9e6c9692ffd16c2cab0
charmilakonada24cse-10 <charmilakonada.24.cse@anits.edu.in>
2026-07-26 18:48:07 +0530

    Add files via upload

---
+++ prg5.java
@@ -0,0 +1,49 @@
+public class Main {
+
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int[] merged = new int[n + m];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < n) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < m) {
+            merged[k++] = arr2[j++];
+        }
+
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        int[] result = mergeSortedArrays(arr1, arr2);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }    }
+}
